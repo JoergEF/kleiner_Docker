aff0f00f75fe5695efa375e85a9826401c6e3767
Jörg Hofmann <joerg@thuenic.de>
2021-06-29 14:58:02 +0200

    Default-Route und DNS auf rt-b

--- scripts/docker-provision.sh
+++ scripts/docker-provision.sh
@@ -10,3 +10,13 @@ apt-get update && apt-get -y dist-upgrade && apt-get --purge -y autoremove
 apt-get -y install docker.io
 systemctl enable --now docker
 adduser vagrant docker
+
+# Default-Route setzen
+ip route replace default via 192.168.100.2
+ip route del default via 10.0.2.2
+
+# Konfiguration der Namensauflösung
+systemctl disable --now systemd-resolved
+rm /etc/resolv.conf
+echo "nameserver 192.168.100.2" > /etc/resolv.conf
+echo "search kurs.iad" >> /etc/resolv.conf
